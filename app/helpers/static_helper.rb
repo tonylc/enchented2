6bbd57be2e4fc665f9ec25fc5e787ea834abebbf
tony <tonylc@gmail.com>
2011-04-18 00:17:08 -0700

    move new menus up

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -3,7 +3,9 @@ module StaticHelper
     links = [[:about, "About Us", about_us_path],
         		 [:event_details, "Event Details", event_details_path],
         		 [:wedding_party, "Wedding Party", wedding_party_path],
-        		 [:registry, "Registry", registry_path]]
+        		 [:rsvp, "RSVP", '#'],
+        		 [:registry, "Registry", registry_path],
+        		 [:blog, "Blog", "#"]]
         		 
     render :layout => "shared/column_nav", :locals => {:selected => selected, :links => links} do
       yield

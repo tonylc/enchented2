24a40f913077b6ae089ccda9f32b3dc600674f84
Eric Lanehart <elanehart@sparkart.com>
2011-06-07 02:37:34 -0700

    Converted styles for RSVP, Guest/Travel, and Events to use new markup. Simplified JavaScript for RSVP page and added "focusing" effect for optional data.

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -5,31 +5,29 @@
 
     $(window).load(function() {
 
-        function mapLocation() {
+        function mapPlaces() {
 
-            if ($(this).hasClass('mapped') || $(this).prev().hasClass('mapped')) {
+            if ($(this).prev().hasClass('map')) {
                 return false;
             }
 
-            if ($(this).children('ul').length > 0) {
-                var latitude1 = parseFloat($(this).children('ul').children('li:first-child').children('.latitude').text());
-                var longitude1 = parseFloat($(this).children('ul').children('li:first-child').children('.longitude').text());
-                var latitude2 = parseFloat($(this).children('ul').children('li:last-child').children('.latitude').text());
-                var longitude2 = parseFloat($(this).children('ul').children('li:last-child').children('.longitude').text());
-                var directions = new google.maps.DirectionsService();
-                var directionsMap;
-                var location = new google.maps.LatLng(latitude1, longitude1);
-                var end = new google.maps.LatLng(latitude2, longitude2);
-            } else {
-                var latitude = parseFloat($(this).children('.latitude').text());
-                var longitude = parseFloat($(this).children('.longitude').text());
-                var location = new google.maps.LatLng(latitude, longitude);
-            }
+            var places = new Array();
 
+            $(this).children('li').each(function(index) {
+
+                var latitude = parseFloat($(this).children('.geo').children('.latitude').text());
+                var longitude = parseFloat($(this).children('.geo').children('.longitude').text());
+
+                places[index] = new Array(latitude, longitude);
+
+            });
+
+            var start = new google.maps.LatLng(places[0][0], places[0][1]);
             var random = Date.parse(new Date());
-            var options = {
+            var $map = $('<div id="map-' + random + '" class="map"></div>').insertBefore($(this));
+            var map = new google.maps.Map($map[0], {
 
-                center: location,
+                center: start,
                 zoom: 13,
                 scrollwheel: false,
                 mapTypeId: google.maps.MapTypeId.TERRAIN,
@@ -37,18 +35,15 @@
                     style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
                 }
 
-            }
+            });
 
-            if ($(this).hasClass('map')) {
-                var map = new google.maps.Map($(this)[0], options);
-            } else {
-                $('<div id="map-' + random + '" class="map mapped"></div>').insertBefore($(this));
-                var map = new google.maps.Map($(this).parent().children('.map')[0], options);
-            }
+            if (places.length > 1) {
 
-            if (directions) {
+                var directions = new google.maps.DirectionsService();
+                var directionsMap;
+                var end = new google.maps.LatLng(places[places.length - 1][0], places[places.length - 1][1]);
                 var to = {
-                    origin: location,
+                    origin: start,
                     destination: end,
                     travelMode: google.maps.DirectionsTravelMode.DRIVING
                 }
@@ -60,27 +55,22 @@
                         directionsMap.setDirections(response);
                     }
                 });
+
             } else {
+
                 var marker = new google.maps.Marker({
-                    position: location,
+                    position: start,
                     map: map
                 });
-            }
 
-            $(this).addClass('mapped');
+            }
 
         }
 
-        $('div.tabbed div:not(.ui-tabs-hide) .location, div.tabbed div:not(.ui-tabs-hide) .map').each(mapLocation);
-        $('.tabbed').bind('tabsshow', function(event, ui) {
+        $('section.tabbed div:not(.ui-tabs-hide) ol.places').each(mapPlaces);
+        $('section.tabbed').bind('tabsshow', function(event, ui) {
 
-            var $this = $('#' + ui.panel.id);
-
-            if ($this.children('.map').length > 0) {
-                $this.children('.map').each(mapLocation);
-            } else {
-                $this.children('.location').each(mapLocation);
-            }
+            $('#' + ui.panel.id).children('ol.places').each(mapPlaces);
 
         });
 

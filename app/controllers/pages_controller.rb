f3be270b999e66157ab397677667b9f66adb4cb5
Eric Lanehart <elanehart@sparkart.com>
2011-07-17 17:03:30 -0700

    Adding layout-less output format

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  
+
   def show
     if @custom_domain
       @page = params[:page_name].blank? ? Page.find_home_page(@wedding) : Page.find_by_wedding_id_and_url_name(@wedding.id, params[:page_name])
@@ -7,16 +7,18 @@ class PagesController < ApplicationController
       @page = Page.find(params[:id])
     end
 
-    
+
     respond_to do |format|
 
-      format.html{
-        
+      format.html {
+      }
+      format.pjax {
+        render :page => @page, :layout => nil
       }
       format.json {
-        render :json => page.to_json(:except => [:is_locked], 
-                                     :include => {:page_widgets => 
-                                       {:include => {:widget => 
+        render :json => page.to_json(:except => [:is_locked],
+                                     :include => {:page_widgets =>
+                                       {:include => {:widget =>
                                            {:include => :location}
                                          }
                                        }

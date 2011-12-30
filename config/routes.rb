a907e0ca06975385739edc44b722242267f36df1
tony <tony@songkick.com>
2011-12-30 19:42:20 +0000

    default with id 1

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,11 @@ Prototype::Application.routes.draw do
   #nice urls
   match ':page_name', :controller => "pages", :action => "show", :via => :get, :as => 'readable_page'
 
-  #home
-  root :to => "pages", :controller => "pages", :action => "show", :via => :get
-
   match "example/:action(/:context)(/:view)", :controller => "example"
   match "proxy", :controller => "proxy"
   match "prototype/:page(/:user)", :controller => "prototype", :action => "display"
   match "ui/:action/:widget(/:view)", :controller => "example"
 
-  root :to => "pages", :controller => "pages", :page => "show", :id => 1
-
+  #home
+  root :to => "pages#show", :id => 1, :via => :get
 end

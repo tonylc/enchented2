f14c2b868d5ba5516b5f689df699c73ecfc82b5f
tony <tony@songkick.com>
2011-12-30 19:39:33 +0000

    default to pages => 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Prototype::Application.routes.draw do
-  
+
   #match 'wedding_party' => 'static#wedding_party'
   #standard request urls
   resources :pages, :only => [:show]
@@ -7,15 +7,15 @@ Prototype::Application.routes.draw do
 
   #nice urls
   match ':page_name', :controller => "pages", :action => "show", :via => :get, :as => 'readable_page'
-  
+
   #home
   root :to => "pages", :controller => "pages", :action => "show", :via => :get
-  
+
   match "example/:action(/:context)(/:view)", :controller => "example"
   match "proxy", :controller => "proxy"
   match "prototype/:page(/:user)", :controller => "prototype", :action => "display"
   match "ui/:action/:widget(/:view)", :controller => "example"
 
-  #root :to => "prototype", :controller => "prototype", :page => "start", :user => "new", :action => "display"
+  root :to => "pages", :controller => "pages", :page => "show", :id => 1
 
 end

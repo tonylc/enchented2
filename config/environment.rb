064abdeab7a6c6465c93af0fcc3fa946b13238b6
tony <tony@songkick.com>
2011-12-30 17:29:55 +0000

    require 'sass'

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
-# Change Sass assets location 
+require 'sass'
+require 'sass/plugin'
+# Change Sass assets location
 Sass::Plugin.options[:template_location] = './public'
 Sass::Plugin.options[:css_location] = './public/prototype/stylesheets'
 Sass::Plugin.options[:style] = :compact
